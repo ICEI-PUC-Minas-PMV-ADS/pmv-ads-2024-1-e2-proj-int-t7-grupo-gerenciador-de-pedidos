fb3d7623ef1005df58f17b2762ca2c95fbca2859
PedroNogueir4 <pedroffn209@gmail.com>
2024-04-23 11:44:14 -0300

    Config Banco de dados e criação da tabela Produtos

---
+++ codígo/backend/backend/Migrations/20240423141509_AddTableProdutos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    descricao = table.Column<string>(type: "text", nullable: false),
+                    valor = table.Column<double>(type: "double precision", nullable: false),
+                    quantidade = table.Column<int>(type: "integer", nullable: false),
+                    categoria = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ codígo/backend/backend/Models/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório adicionar um nome!")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório adicionar uma descrição!")]
+        public string descricao { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório adicionar um valor!")]
+        public double valor { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório adicionar uma quantidade!")]
+        public int quantidade { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório adicionar uma categoria!")]
+        public string categoria { get; set; }
+
+    }
+}
