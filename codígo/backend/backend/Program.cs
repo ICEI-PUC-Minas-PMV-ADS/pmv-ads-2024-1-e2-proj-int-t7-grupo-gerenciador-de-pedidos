33edddc7508bdc6826a7c2254fe9c4506b08a612
PedroNogueir4 <pedroffn209@gmail.com>
2024-04-30 15:47:49 -0300

    Atualização Banco de dados

--- codígo/backend/backend/Program.cs
+++ codígo/backend/backend/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
