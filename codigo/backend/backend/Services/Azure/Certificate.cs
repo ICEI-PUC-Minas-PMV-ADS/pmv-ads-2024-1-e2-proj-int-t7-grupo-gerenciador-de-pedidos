3afc4b135b9b254ccb22d02ca07bebdce63a59ce
Magnoamaury <144865205+AmauryMagno@users.noreply.github.com>
2024-06-05 16:09:05 -0300

    Ajuste area Cliente

--- codigo/backend/backend/Services/Azure/Certificate.cs
+++ codigo/backend/backend/Services/Azure/Certificate.cs
@@ -1,10 +1,13 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+
 namespace backend.Services.Azure
 {
 
     public class Certificate
     {
-        static readonly string Key = "sp=r&st=2024-06-03T03:37:29Z&se=2024-12-04T02:30:00Z&spr=https&sv=2022-11-02&sr=c&sig=9b76%2FQwyP%2FdVg6%2B7qZQ3L4wBLDzSBsxobX7fMq5QOWU%3D";
+        static readonly string Key = "sp=r&st=2024-06-01T15:00:00Z&se=2024-12-31T02:55:00Z&spr=https&sv=2022-11-02&sr=c&sig=VxC6hllQvJi2Cor0HSF1fnljALMC2gmQPtC1r5a%2FWHs%3D";
 
         public string AcessImgBlob(string fileUrl, string FileName)
         {
